1aa162b94bb88a426add82100dcd3053b75ae1bd
ben.heo <notear0@naver.com>
2019-04-11 23:43:22 +0900

    just commit...

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -22,4 +22,40 @@ train_df = pd.read_csv('../input/train.csv')
 test_df = pd.read_csv('../input/test.csv')
 combine = [train_df, test_df]
 
-print(train_df.columns.values)
+# print(train_df.columns.values)
+
+# preview the data
+train_df.head()
+train_df.tail()
+
+train_df.info()
+print('_'*40)
+test_df.info()
+
+train_df.describe()
+# Review survived rate using `percentiles=[.61, .62]` knowing our problem description mentions 38% survival rate.
+# Review Parch distribution using `percentiles=[.75, .8]`
+# SibSp distribution `[.68, .69]`
+# Age and Fare `[.1, .2, .3, .4, .5, .6, .7, .8, .9, .99]`
+
+train_df.describe(include=['O'])
+
+train_df[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean().sort_values(by='Survived', ascending=False)
+
+g = sns.FacetGrid(train_df, col='Survived')
+g.map(plt.hist, 'Age', bins=20)
+
+# grid = sns.FacetGrid(train_df, col='Pclass', hue='Survived')
+grid = sns.FacetGrid(train_df, col='Survived', row='Pclass', size=2.2, aspect=1.6)
+grid.map(plt.hist, 'Age', alpha=.5, bins=20)
+grid.add_legend();
+
+# grid = sns.FacetGrid(train_df, col='Embarked')
+grid = sns.FacetGrid(train_df, row='Embarked', size=2.2, aspect=1.6)
+grid.map(sns.pointplot, 'Pclass', 'Survived', 'Sex', palette='deep')
+grid.add_legend()
+
+# grid = sns.FacetGrid(train_df, col='Embarked', hue='Survived', palette={0: 'k', 1: 'w'})
+grid = sns.FacetGrid(train_df, row='Embarked', col='Survived', size=2.2, aspect=1.6)
+grid.map(sns.barplot, 'Sex', 'Fare', alpha=.5, ci=None)
+grid.add_legend()
